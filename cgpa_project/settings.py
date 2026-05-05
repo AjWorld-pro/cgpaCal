32ddeb57e269a439a26487df3809e4534834ee38
AjWorld-pro <155429504+AjWorld-pro@users.noreply.github.com>
2026-05-05 13:17:09 +0100

    svysf

--- cgpa_project/settings.py
+++ cgpa_project/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-^9l3ek9$y-n&mdg@^52fne2lm-epm8)_#2mw523m59l+099yv3')
 
 # 🔴 PRODUCTION FIX
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
